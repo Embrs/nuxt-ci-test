9e5b169b345902fad7ef232db352aa73d1539d91
Harry <ddembrs@gmail.com>
2025-05-05 14:45:45 +0800

    backup

--- app.vue
+++ app.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <p> TEST </p>
+    <p> {{ msg }}</p>
+      <!-- <NuxtRouteAnnouncer />
+      <NuxtWelcome /> -->
   </div>
 </template>
+
+<script setup>
+
+onMounted(async() => {
+  const msg = ref('');
+  console.log('onMounted')
+  const { data } = await useFetch('http://backend:8000')
+  msg.value = data.value?.message
+})
+</script>
